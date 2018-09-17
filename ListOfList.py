055a36bf130a1e8094143b330acb57f926668002
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-09-17 07:59:21 +0530

    Create ListOfList.py

---
+++ ListOfList.py
@@ -0,0 +1,14 @@
+Planets = ["Earth","Venus","mercury"]
+ Galaxy = ["milkyway","andromeda","somethingelse"]
+ Stars = ["titan","galieo","gargantula"]
+ the_list = []
+ the_list.append(Planets)
+ the_list.append(Galaxy)
+ the_list.append(Stars)
+ print(the_list)
+
+ the_list[0][1]
+ the_list[1][2]
+ the_list[2][3]="newname"
+
+ print(the_list) 
