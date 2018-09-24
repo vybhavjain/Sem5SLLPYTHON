cb77a9052d57148a2180fa15e7021088bd46f10f
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-09-24 09:38:20 +0530

    Update createperson.py

--- createperson.py
+++ createperson.py
@@ -1,6 +1,8 @@
+# or can also be :
+
 class person:
  def __init__(self,name,age):  #constructor because of __init__ 
-  self.name = name;
+  self.name = name;    # self can be replaced anytime with another word in the constructor defintion itself
   self.age = age;
 
 p1 = person("suppandi",14)
@@ -15,3 +17,28 @@ print " \n Age of person 2 is : ", p2.age
 p2.age = 10
 
 print " \n Age of person 2 is : ", p2.age
+
+
+"""  multicomment line 
+ 
+class person:
+ def __init__(vybhav,name,age):  #constructor because of __init__ 
+  vybhav.name = name;
+  vybhav.age = age;
+
+p1 = person("suppandi",14)
+p2 = person("Ramu",12)
+
+print " \n Name of person 1 is : ", p1.name
+print " \n Age of person 1 is : ", p1.age
+
+print " \n Name of person 2 is : ", p2.name
+print " \n Age of person 2 is : ", p2.age
+
+p2.age = 10
+
+print " \n Age of person 2 is : ", p2.age
+
+
+"""
+
